e45aca24fb75ca5a4b2a22b2c50f760f65d1ce1b
BeneDevRepo <benedikt.binger@gmail.com>
2022-11-16 16:25:51 +0100

    Update DB3 Aufgabe9

--- Datenbanken/Aufgabe9/src/DbCLI.cpp
+++ Datenbanken/Aufgabe9/src/DbCLI.cpp
@@ -1,5 +1,7 @@
 #include "DbCLI.hpp"
 
+#include "Parser.hpp"
+
 #include "util.hpp"
 
 
@@ -41,6 +43,16 @@ void DbCLI::run() {
 }
 
 DbCLI::COMMAND_STATE DbCLI::checkCMD(const std::string& input) const {
+	struct MatchResult {
+		bool success;
+		std::string error;
+		size_t index; // problematic index
+		inline MatchResult():
+			success(true) {}
+		inline MatchResult(const size_t index, const std::string& error):
+			success(false), error(error), index(index) { }
+	};
+
 	struct Token {
 		enum Type {
 			LITERAL,
@@ -50,15 +62,24 @@ DbCLI::COMMAND_STATE DbCLI::checkCMD(const std::string& input) const {
 		std::string _content;
 		static inline Token Literal(const std::string& literal) { return { .type = LITERAL, ._content = literal }; }
 		static inline Token Name() { return { .type = NAME }; }
-		inline void match(const std::string& command, size_t index) {
+		inline MatchResult test(const std::string& command, size_t index) {
+			constexpr auto isLetter = [](const char c){ return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'); };
+			constexpr auto isNumber = [](const char c){ return c >= '0' && c <= '9'; };
 			switch(type) {
 				case LITERAL:
-					break;
+					for(size_t i = 0; i < _content.size(); i++)
+						if(command [index + i] != _content[i])
+							return MatchResult(index + i, "Substitution failed: missmatched literal");
+					return MatchResult();
+
 				case NAME:
+					for(size_t i = 0; isLetter(command[index + i]) || (i > 0 && isNumber(index + i)); i++)
+						;
 					break;
 			}
 		}
 	};
+
 	static const std::string SELECT = "SELECT (column+:column|*) FROM Buch";
 	size_t index;
 	for(;;) {
