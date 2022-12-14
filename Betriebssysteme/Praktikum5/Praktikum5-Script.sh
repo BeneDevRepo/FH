a332d4455f9e985695f23a81cd25557c303281ac
benedevrepo <benedikt.binger@gmail.com>
2022-12-14 08:53:00 +0100

    Update

--- Betriebssysteme/Praktikum5/Praktikum5-Script.sh
+++ Betriebssysteme/Praktikum5/Praktikum5-Script.sh
@@ -22,23 +22,29 @@ outfile="report.txt"
 
 # Clear output file
 echo "" > "$outfile"
-if ! echo "" > "$outfile" &> /dev/null; then
+if ! echo "" >> "$outfile" &> /dev/null; then
 	echo "Kann nicht in outputdatei schreiben."
 	exit 1
 fi
 
 
 # 5.6.1:  Verzeichnisse nach Groesse sortiert auflisten
-echo "\nDirectories in / ordered by size, descending:" > "$outfile"
+printf "\nDirectories in / ordered by size, descending:\n" >> "$outfile"
 du -S -d1 / 2>/dev/null | sort -nr >> "$outfile"
 
 
 # 5.6.2:  Dateien nach Groesse sortiert auflisten
-echo "\nFiles in /usr/bin ordered by size, descending:" > "$outfile"
+printf "\nFiles in /usr/bin ordered by size, descending:\n" >> "$outfile"
 ls -lsF /usr/bin | grep "*" | sort -nr >> "$outfile"
 
 
 # 5.6.3:  Dateien ohne Besitzer
-echo "\nFiles in / without user:" > "$outfile"
-cd /
-find -maxdepth 1 -nouser | sort >> "$outfile"
+printf "\nFiles in / without user:\n" >> "$outfile"
+#cd /
+find / -maxdepth 1 -nouser | sort >> "$outfile"
+
+
+
+# 5.6.4:  SUID oder SGID
+printf "\nFiles in / with SUID or GUID set\n" >> "$outfile"
+find / -type f -executable -perm /6000 -ls 2>/dev/null >> "$outfile"
